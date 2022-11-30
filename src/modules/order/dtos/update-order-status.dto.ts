163d3116c7fb24e24a0362767ac45377a7886183
Lara Beatriz <31808723+larabeatrizms@users.noreply.github.com>
2022-11-30 15:50:43 -0300

    feat: customer users can cancel order

--- src/modules/order/dtos/update-order-status.dto.ts
+++ src/modules/order/dtos/update-order-status.dto.ts
@@ -1,6 +1,8 @@
+import { IUserSession } from 'src/shared/interfaces/user-session.interface';
 import { EOrderStatusCode } from '../enums/order-status-code.enum';
 
 export interface UpdateOrderStatusDto {
   id: number;
   status: EOrderStatusCode;
+  userSession: IUserSession;
 }
--- src/modules/order/services/update-order-status.service.ts
+++ src/modules/order/services/update-order-status.service.ts
@@ -4,6 +4,7 @@ import { RpcException } from '@nestjs/microservices';
 import { UpdateOrderStatusDto } from '../dtos/update-order-status.dto';
 import { ISuccessResponse } from 'src/shared/interfaces/SuccessResponse.interface';
 import { OrderRepositoryInterface } from '../repositories/order/order.interface.repository';
+import { EOrderStatusCode } from '../enums/order-status-code.enum';
 
 export class UpdateOrderService {
   private readonly logger = new Logger(UpdateOrderService.name);
@@ -19,12 +20,48 @@ export class UpdateOrderService {
 
       this.logger.log('Validating fields...');
 
-      const { id, status } = data;
+      const { id, status, userSession } = data;
 
-      const order = await this.orderRepository.findOneById(id);
+      const order = await this.orderRepository.findOneWithRelations({
+        where: {
+          id,
+        },
+        relations: {
+          customer: true,
+        },
+      });
 
       if (!order) {
         throw new RpcException('Pedido não encontrado!');
+      }
+
+      if (
+        userSession.role === 'customer' &&
+        order.customer.integration_id !== userSession.userId
+      ) {
+        throw new RpcException('Usuário sem permissão de realizar esta ação.');
+      }
+
+      if (
+        userSession.role === 'customer' &&
+        status !== EOrderStatusCode.CANCELLED
+      ) {
+        throw new RpcException(
+          'Usuário com permissão apenas de cancelar pedido.',
+        );
+      }
+
+      const canCacelled = [
+        EOrderStatusCode.PLACED,
+        EOrderStatusCode.CONFIRMED,
+        EOrderStatusCode.PREPARATION_STARTED,
+      ];
+
+      if (
+        userSession.role === 'customer' &&
+        !canCacelled.includes(order.status)
+      ) {
+        throw new RpcException('Não é possível cancelar o pedido nesta etapa.');
       }
 
       order.status = status;
